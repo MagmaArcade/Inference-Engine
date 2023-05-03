f940fe4fb72ec603645e24ec0d60debbd17d853f
nightbroker <nicholasgustin1@gmail.com>
2023-05-03 13:13:14 +1000

    Recreated Visual Studio directory to include top level statements

---
+++ InferenceEngine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InferenceEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
