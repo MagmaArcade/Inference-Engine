edeb9fa2364ba5e472ac676be967d8cf68f6e43f
Nicholas <nicholasgustin1@gmail.com>
2023-05-09 22:25:53 +1000

    Changed Visual Studio file to .NET 4.8
    As per assignment brief

--- InferenceEngine/Program.cs
+++ InferenceEngine/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace InferenceEngine
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
